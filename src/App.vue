0705407b3a5e2a33e91bac1970693e0bc23661a1
jumagu <jmgg.gutierrez@gmail.com>
2024-09-18 14:06:29 -0500

    Refactor App.vue to use IndecisionView component for messaging functionality

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <script setup lang="ts">
-import MyCounter from './components/MyCounter.vue';
-import MyCounterScript from './components/MyCounterScript.vue';
-import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue';
+import IndecisionView from './views/IndecisionView.vue';
 </script>
 
 <template>
-  <h1 class="text-3xl">My first Vue application</h1>
-
-  <hr />
-
-  <MyCounter :value="1" text="Counter script setup" />
-  <MyCounterScript :value="2" text="Counter traditional component" />
-  <MyCounterScript2 :value="3" text="Counter separed logic" />
+  <IndecisionView />
 </template>
