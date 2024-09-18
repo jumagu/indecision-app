83d0b8794307db4c6a3bc2e0aba334140913b136
jumagu <jmgg.gutierrez@gmail.com>
2024-09-18 14:06:07 -0500

    Add chat components for messaging functionality

---
+++ src/components/chat/ChatMessageBubble.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface ChatMessageBubbleProps {
+  media?: string;
+  message: string;
+  isSender: boolean;
+}
+
+defineProps<ChatMessageBubbleProps>();
+</script>
+
+<template>
+  <!-- Sended Message -->
+  <div v-if="isSender" class="flex justify-end">
+    <div class="bg-blue-200 text-black p-2 rounded-lg max-w-xs">{{ message }}</div>
+  </div>
+
+  <!-- Received Message -->
+  <div v-else class="flex">
+    <div class="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
+      <p class="capitalize">{{ message }}</p>
+      <img v-if="media" :src="media" alt="YesNoImage" class="w-52 h-52 object-cover rounded" />
+    </div>
+  </div>
+</template>
---
+++ src/components/chat/ChatMessages.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import ChatMessageBubble from './ChatMessageBubble.vue';
+import type { IChatMessage } from '@/interfaces/chat-message.interface';
+
+interface IChatMessagesProps {
+  messages: IChatMessage[];
+}
+
+defineProps<IChatMessagesProps>();
+</script>
+
+<template>
+  <div class="flex-1 overflow-y-auto p-4">
+    <div class="flex flex-col space-y-2">
+      <ChatMessageBubble v-for="message in messages" :key="message.id" v-bind="message" />
+    </div>
+  </div>
+</template>
