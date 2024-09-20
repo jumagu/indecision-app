91b363049a4bf255185b241d8de52bcff747ac47
jumagu <jmgg.gutierrez@gmail.com>
2024-09-19 19:02:29 -0500

    Refactor useCounter to calculate squareCounter correctly

--- src/composables/useCounter.ts
+++ src/composables/useCounter.ts
@@ -1,14 +1,8 @@
 import { computed, ref } from 'vue';
 
-// ? Súper poder de Vue!
-// Crear está varibale globalmente quiere decir que será usada
-// en todos los lugares donde se llame useCounter.
-// Es como un estado global!!!
-// const counter = ref(10);
-
 export const useCounter = (initialValue: number = 5) => {
   const counter = ref(initialValue);
-  const squareCounter = computed(() => counter.value);
+  const squareCounter = computed(() => counter.value * counter.value);
 
   return { counter, squareCounter };
 };
