6ab367df330b55e4ea879a3b2010fa671bb168b5
jumagu <jmgg.gutierrez@gmail.com>
2024-09-22 17:42:39 -0500

    feat: add delete chat FAB style: add avatar

--- src/views/IndecisionView.vue
+++ src/views/IndecisionView.vue
@@ -2,14 +2,30 @@
 import { useChat } from '@/composables/useChat';
 import ChatMessages from '@/components/chat/ChatMessages.vue';
 import ChatMessageBox from '@/components/chat/ChatMessageBox.vue';
+import TrashCanIcon from '@/components/icons/TrashCanIcon.vue';
 
-const { messages, isLoading, sendMessageHandler } = useChat();
+const { messages, isLoading, clearChat, sendMessageHandler } = useChat();
 </script>
 
 <template>
   <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
     <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
-      <span>Anonymous</span>
+      <div>
+        <img
+          class="inline-block h-9 w-9 rounded-full object-cover ring-2 ring-white"
+          src="../../public/anonymous.jpg"
+          alt="Anonymous"
+        />
+        <span class="ml-2">Anonymous</span>
+      </div>
+
+      <button
+        class="bg-red-500 text-white rounded-full p-2 ml-2 hover:bg-red-600 focus:outline-none disabled:opacity-80 disabled:pointer-events-none"
+        :disabled="!messages.length"
+        @click="clearChat"
+      >
+        <TrashCanIcon />
+      </button>
     </div>
 
     <!-- Messages Section -->
